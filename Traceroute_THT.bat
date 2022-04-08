547061c0b1ad5bfef2d98b972c48d8df10b5761e
MTT768 <93826344+MTT768@users.noreply.github.com>
2022-04-08 23:30:00 +0300

    Update Traceroute_THT.bat

--- Traceroute_THT.bat
+++ Traceroute_THT.bat
@@ -1,6 +1,6 @@
 @echo off
 title Traceroute THT
-echo Bu program MuhammedTr768 Tarafindan yapilmistir.
+echo Bu program MT768 Tarafindan yapilmistir.
 set/p yaz=IP Veya URL Girin:
 tracert %yaz%
 echo Herhangi bir tusa basarak kapatabilirsiniz.
